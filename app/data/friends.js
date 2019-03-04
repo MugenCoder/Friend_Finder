827986e37b2d9f6861c921eb296278e657cd7c72
Mugencoder <cchavis3@gmail.com>
2019-03-03 20:04:27 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,124 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. Each of these objects should roughly follow the format below.
+var mateArray = {
+  "name":"Lion-O",
+  "photo":"../images/Lion-O-Ritchie.jpg",
+  "scores":[
+      3,
+      2,
+      5,
+      3,
+      4,
+      2,
+      1,
+      3,
+      4,
+      1
+    ],
+
+      "name":"Cheetara",
+      "photo":"../images/Cheetara.jpg",
+      "scores":[
+          5,
+          3,
+          4,
+          4,
+          3,
+          5,
+          1,
+          3,
+          5,
+          4
+        ],
+
+        "name":"Panthro",
+      "photo":"../images/Panthro.jpg",
+      "scores":[
+          5,
+          3,
+          4,
+          4,
+          5,
+          3,
+          4,
+          5,
+          4,
+          3
+        ],
+
+        "name":"Tygra",
+      "photo":"../images/Tygra.jpg",
+      "scores":[
+          4,
+          5,
+          3,
+          3,
+          4,
+          1,
+          1,
+          4,
+          3,
+          1
+        ],
+
+        "name":"Mumra",
+      "photo":"../images/Mumrah.jpg",
+      "scores":[
+          1,
+          3,
+          5,
+          4,
+          2,
+          2,
+          2,
+          3,
+          4,
+          1
+        ],
+
+        "name":"Chilla",
+      "photo":"../images/Chilla.jpg",
+      "scores":[
+          4,
+          3,
+          3,
+          2,
+          5,
+          4,
+          3,
+          5,
+          4,
+          2
+        ],
+
+        "name":"Snarf",
+      "photo":"../images/Snarf.jpg",
+      "scores":[
+          3,
+          1,
+          2,
+          2,
+          3,
+          1,
+          2,
+          5,
+          4,
+          1
+        ],
+
+       
+        "name":"Pumyra",
+      "photo":"../images/Tygra.jpg",
+      "scores":[
+          4,
+          3,
+          4,
+          4,
+          3,
+          5,
+          2,
+          2,
+          1,
+          5
+        ] 
+};
+
