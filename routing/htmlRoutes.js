86f98826608ff655b6bfff3ae2accab6fad02e3f
Mugencoder <cchavis3@gmail.com>
2019-10-08 23:16:54 -0400

    optimized html routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,21 +4,19 @@
 // A default, catch-all route that leads to home.html which displays the home page.
 
 // DEPENDENIES
+
 const path = require('path');
 
 // ROUTING
+
 module.exports = function(app) {
 // HTML GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases the user is shown an HTML page of content
     // ---------------------------------------------------------------------------
   
-    app.get("/", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-  
     app.get("/survey", function(req, res) {
-      res.sendFile(path.join(__dirname, "../public/survey.html"));
+      res.sendFile(path.join(__dirname, "../public/home.html"));
     });
   
     // If no matching route is found default to home
